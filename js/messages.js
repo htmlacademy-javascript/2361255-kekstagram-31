2588b4e0ff7a1b26677e42292d28f08dc2615e6e
Валерия Рогачева <valerie55@mail.ru>
2024-04-04 00:34:44 +0300

    11.14

---
+++ js/messages.js
@@ -0,0 +1,54 @@
+import { isEscapeKey } from './util.js';
+
+
+// Ищем образец сообщений в разметке
+const messageError = document.querySelector('#error').content.querySelector('.error');
+const messageSuccess = document.querySelector('#success').content.querySelector('.success');
+
+// Функция, скрывающая сообщение
+const hideMessage = () => {
+  const exitMessage = document.querySelector('.success') || document.querySelector('.error');
+  exitMessage.remove();
+  document.removeEventListener('keydown', onDocumentKeydown);
+  document.body.removeEventListener('click', onBodyClick);
+};
+
+// Скрытие сообщения при нажатии на кнопку закрытия
+const onCloseButtonClick = () => hideMessage();
+
+// Скрытие сообщения при нажатии на Esc
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    hideMessage();
+  }
+}
+
+// Скрытие сообщения при нажатии на область вокруг сообщения
+function onBodyClick(evt) {
+  if (evt.target.closest('.success__inner') || (evt.target.closest('.error__inner'))) {
+    return;
+  }
+
+  hideMessage();
+}
+
+// Показ сообщения
+const showMessage = (element, buttonClass) => {
+  document.body.append(element);
+  document.body.addEventListener('click', onBodyClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+  element.querySelector(buttonClass).addEventListener('click', onCloseButtonClick);
+};
+
+// Показ сообщения с ошибкой
+const showErrorMessage = () => {
+  showMessage(messageError, '.error__button');
+};
+
+// Показ сообщения с успехом
+const showSuccessMessage = () => {
+  showMessage(messageSuccess, '.success__button');
+};
+
+export { showErrorMessage, showSuccessMessage };
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,4 +1,4 @@
-import { arrayPhotos } from './data.js';
+// import { arrayPhotos } from './data.js';
 import { openBigPicture } from './fulls-photos.js';
 
 // Ищем шаблон
@@ -7,36 +7,38 @@ const templateUserPicture = document.querySelector('#picture').content.querySele
 //контейнер
 const containerUsersPictures = document.querySelector('.pictures');
 
-const usersPicturesFragment = document.createDocumentFragment();
+// const usersPictures = arrayPhotos();
 
-const usersPictures = arrayPhotos();
+const createPosts = (usersPictures) => {
+  const usersPicturesFragment = document.createDocumentFragment();
+  usersPictures.forEach(({ url, description, likes, comments, id }) => {
+    //клон шаблона
+    const userPicture = templateUserPicture.cloneNode(true);
 
-usersPictures.forEach(({ url, description, likes, comments, id }) => {
-  //клон шаблона
-  const userPicture = templateUserPicture.cloneNode(true);
+    //вставляем данные в шаблон
+    userPicture.querySelector('.picture__img').src = url;
+    userPicture.querySelector('.picture__img').alt = description;
+    userPicture.querySelector('.picture__likes').textContent = likes;
+    userPicture.querySelector('.picture__comments').textContent = comments.length;
+    userPicture.dataset.id = id;
 
-  //вставляем данные в шаблон
-  userPicture.querySelector('.picture__img').src = url;
-  userPicture.querySelector('.picture__img').alt = description;
-  userPicture.querySelector('.picture__likes').textContent = likes;
-  userPicture.querySelector('.picture__comments').textContent = comments.length;
-  userPicture.dataset.id = id;
+    usersPicturesFragment.appendChild(userPicture);
+  });
 
-  usersPicturesFragment.appendChild(userPicture);
-});
+  containerUsersPictures.appendChild(usersPicturesFragment);
 
-containerUsersPictures.appendChild(usersPicturesFragment);
+  containerUsersPictures.addEventListener('click', (evt) => {
+    const picture = evt.target.closest('.picture');
 
+    if (picture) {
+      evt.preventDefault();
+      //в массиве находим фото, id = id, по которому произошел клик
+      const dataPicture = usersPictures.find((item) => picture.dataset.id === item.id.toString());
 
-containerUsersPictures.addEventListener('click', (evt) => {
-  const picture = evt.target.closest('.picture');
+      openBigPicture(dataPicture);
+    }
+  });
+};
 
-  if (picture) {
-    evt.preventDefault();
-    //в массиве находим фото, id = id, по которому произошел клик
-    const dataPicture = usersPictures.find((item) => picture.dataset.id === item.id.toString());
-
-    openBigPicture(dataPicture);
-  }
-});
+export { createPosts };
 
